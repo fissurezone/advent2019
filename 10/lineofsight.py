22d12e913e74ad2c88675fbbe2b6157c38f3a37d
Kunal Chowdhury <kunal.chowdhury@gmail.com>
2020-01-26 22:33:41 +0530

    added day 10 part 1

--- 10/lineofsight.py
+++ 10/lineofsight.py
@@ -1,4 +1,45 @@
-import itertools
+from functools import reduce
+from operator import mul
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+
+
+def prod(iterable):
+    return reduce(mul, iterable, 1)
+
+
+def prime_factorial(n):
+    n = abs(n)
+    if n == 1:
+        yield 1
+    else:
+        for i in primes:
+            if i * i > n:
+                break
+            while n % i == 0:
+                n //= i
+                yield i
+            if n == 1:
+                break
+        if n > 1:
+            yield n
+
+
+def unit_slope(dx, dy):
+    sign_x = 1 if dx > 0 else -1
+    sign_y = 1 if dy > 0 else -1
+    if dx == 0:
+        return 0, sign_y
+    if dy == 0:
+        return sign_x, 0
+    dx = list(prime_factorial(dx))
+    dy = list(prime_factorial(dy))
+    for i in dx[:]:
+        if i in dy:
+            dx.remove(i)
+            dy.remove(i)
+    return sign_x * prod(dx), sign_y * prod(dy)
+
 
 if __name__ == '__main__':
     matrix = []
@@ -11,31 +52,12 @@ if __name__ == '__main__':
     height = len(matrix)
     width = len(matrix[0])
     asteroids = set((x, y) for y in range(height) for x in range(width) if matrix[y][x])
-    print(asteroids)
 
     detections = {tup: 0 for tup in asteroids}
     for (x, y) in asteroids:
         other_asteroids = asteroids - set(((x, y),))
-        axial_visibility_flags = {'vertical': [0, 0], 'horizontal': [0, 0]}
+        slopes = set(unit_slope(ox - x, oy - y) for (ox, oy) in other_asteroids)
+        detections[(x, y)] = len(slopes)
 
-        for idx, axial_iterable in enumerate(range(x), range(x+1, width)):
-            for vertical_index in axial_iterable:
-                if (vertical_index, y) in other_asteroids:
-                    axial_visibility_flags['vertical'][idx] = 1
-                    other_asteroids.remove((vertical_index, y))
-
-        for idx, axial_iterable in enumerate(range(y), range(y+1, width)):
-            for horizontal_index in axial_iterable:
-                if (x, horizontal_index) in other_asteroids:
-                    axial_visibility_flags['horizontal'][idx] = 1
-                    other_asteroids.remove((x, horizontal_index))
-
-        detections[(x, y)] += sum(sum(flags) for flags in axial_visibility_flags.values())
-
-        while other_asteroids:
-            (other_x, other_y) = other_asteroids.pop()
-            delta_x = x - other_x
-            delta_y = y - other_y
-
-
-
+    max_visibility_location = max(detections, key=detections.get)
+    print('Part 1 : {}'.format(detections[max_visibility_location]))
